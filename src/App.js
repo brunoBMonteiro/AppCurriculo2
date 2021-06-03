e68d4bcee287a372026b968ee1ce4e1f9d93d58d
brunoBMonteiro <brunobilheri@hotmail.com>
2021-06-03 15:25:02 -0300

    continuando criação do app curriculo

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import {
   Image,
   Text
 } from 'react-native';
-import Icon from 'react-native-vector-icons/Feather';
-
+import Icon from 'react-native-vector-icons/Feather'
 import foto from './assets/foto.jpeg';
 
 const App = () => {
@@ -15,12 +14,12 @@ const App = () => {
       <View style={styles.page}>
           <View style={styles.container_header}>
             <Image source={foto} style={styles.foto} />
-            <Text style={styles.name}>Bruno Bilheri</Text>
+            <Text style={styles.name}>Bruno Bilheri Monteiro</Text>
             <Text style={styles.jobDescription}>Desenvolvedor Mobile</Text>
-            <View>
-              <Icon name="github" />
-              <Text>Facebook</Text>
-              <Text>LinkedIn</Text>
+            <View style={styles.social_icons}>
+              <Icon name="github" size={30} />
+              <Icon name="facebook" size={30} />
+              <Icon name="linkedin" size={30} />
             </View>
           </View>
       </View>
@@ -39,18 +38,28 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   foto: {
-    width: 250,
-    height: 250,
+    width: 200,
+    height: 200,
     borderRadius: 125,
   },
   name: {
-    fontSize: 26,
+    fontSize: 22,
     fontWeight: 'bold',
     marginTop: 10
   },
   jobDescription: {
+    fontSize: 15,
     color: '#939393',
-    marginBottom: 10
+    marginBottom: 10,
+    marginTop: 5,
+  },
+  social_icons: {
+    marginTop: 10,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    width: '60%',
+    justifyContent: 'space-evenly',
+    borderRadius: 10,
   },
 })
 
